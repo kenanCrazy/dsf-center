2b7093eb3557dcf005c70a9fdd72033af00313c8
rmi <rmi-jx@jxinternet.com>
2019-01-18 18:10:32 +0800

    first commit

---
+++ dsf-center-bet/src/main/java/com/invech/platform/bet/DsfCenterBetApplication.java
@@ -0,0 +1,13 @@
+package com.invech.platform.bet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsfCenterBetApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DsfCenterBetApplication.class,args);
+  }
+
+}
---
+++ dsf-center-service/src/main/java/com/invech/platform/service/DsfCenterApplication.java
@@ -0,0 +1,12 @@
+package com.invech.platform.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsfCenterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DsfCenterApplication.class,args);
+  }
+}
