49eeca914211b41afb425b61568495da5a3c22bf
1405733736@qq.com <xuyun1225>
2019-01-21 01:04:47 +0800

    dsf-center eureka 配置

---
+++ erueka-server/src/main/java/com/invech/platform/eruekaserver/EruekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.invech.platform.eruekaserver;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EruekaServerApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(EruekaServerApplication.class, args);
+        new SpringApplicationBuilder(EruekaServerApplication.class).web(WebApplicationType.SERVLET).run(args);
+
+    }
+
+}
+
