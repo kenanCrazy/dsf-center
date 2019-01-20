4a9820a33ee5fec3fa93f815f4abc63158c560dc
1405733736@qq.com <xuyun1225>
2019-01-21 01:23:06 +0800

    dsf-center eureka 配置

--- dsf-center-service/src/main/java/com/invech/platform/DsfCenterServiceApplication.java
+++ dsf-center-service/src/main/java/com/invech/platform/DsfCenterServiceApplication.java
@@ -3,11 +3,9 @@ package com.invech.platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@EnableFeignClients
-@EnableEurekaClient
+//@EnableFeignClients
+//@EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
 public class DsfCenterServiceApplication {
