9f90ab6b6c1af69ee6fbd483fc9e6c170b20738a
rmi <rmi-jx@jxinternet.com>
2019-01-30 16:04:38 +0800

    注释

---
+++ dsf-center-service/src/main/java/com/invech/platform/dsfcenterservice/controller/site/SiteCodeController.java
@@ -0,0 +1,33 @@
+package com.invech.platform.dsfcenterservice.controller.site;
+
+import com.invech.platform.dsfcenterdata.utils.AESUtil;
+import com.invech.platform.dsfcenterdata.utils.response.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/site")
+@Api(value = "SiteCode", description = "站点有关")
+public class SiteCodeController {
+
+  @GetMapping("/")
+  @ApiOperation(value = "获取当前站点对应的数据库加密信息", notes = "获取当前站点对应的数据库加密信息")
+  @ApiImplicitParams({@ApiImplicitParam(name = "token", value = "token头部，随便填数字", required = true, dataType = "Integer", paramType = "header") })
+  public R querySiteCode(@RequestParam("webDomain") String webDomain){
+    // 域名对应多个站点,获取站点,再通过站点获取数据库前缀
+
+    String schemaName = "106";
+    try {
+      return R.ok().put("SToken", AESUtil.encrypt(schemaName));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return R.ok();
+  }
+}
