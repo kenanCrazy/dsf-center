e92bdce962ac2f8b876c8c26790bdef87416fe42
rmi <rmi-jx@jxinternet.com>
2019-02-18 18:35:54 +0800

    dsf-center-service 对外提供的api功能

---
+++ dsf-center-dao/src/main/java/com/invech/platform/dsfcenterdao/mapper/DsfStatementsMonthMapper.java
@@ -0,0 +1,10 @@
+package com.invech.platform.dsfcenterdao.mapper;
+
+import com.invech.platform.dsfcenterdao.config.MybatisAutoMapper;
+import com.invech.platform.dsfcenterdata.entity.DsfStatementsMonth;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DsfStatementsMonthMapper extends MybatisAutoMapper<DsfStatementsMonth> {
+
+}
---
+++ dsf-center-dao/src/main/java/com/invech/platform/dsfcenterdao/mapper/DsfStatementsPlayerDayMapper.java
@@ -0,0 +1,10 @@
+package com.invech.platform.dsfcenterdao.mapper;
+
+import com.invech.platform.dsfcenterdao.config.MybatisAutoMapper;
+import com.invech.platform.dsfcenterdata.entity.schema.DsfStatementsPlayerDay;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DsfStatementsPlayerDayMapper extends MybatisAutoMapper<DsfStatementsPlayerDay> {
+
+}
